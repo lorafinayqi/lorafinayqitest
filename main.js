0c0ee4e0f69be5bf11a783e77838928c612b0b93
lorafinayqi <afripow@gmail.com>
2022-05-15 00:13:00 +0700

    test web

---
+++ main.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin:2,
+    nav: true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+  })
